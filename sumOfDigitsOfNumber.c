5141637dfd0647460fc8099af1efaba0b98c21df
shivamsharma2509 <56030049+shivamsharma2509@users.noreply.github.com>
2019-10-01 19:21:13 +0530

    Update sumOfDigitsOfNumber.c

--- sumOfDigitsOfNumber.c
+++ sumOfDigitsOfNumber.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main(void ) {
- int num=0,sum=0;
- int tem=0;
+ int num,sum=0,r;
  printf("Enter number\n");
  scanf("%d", &num);
- tem=num;
-for(int i=0;tem>0;i++)
-{
-  sum=sum+tem%10;
-  tem=tem/10;
-}
+ while(num!=0)
+ {
+  r=num%10;
+  sum+=r;
+  num/=10;
+ } 
+
  printf("sum of digits of %d = %d",num,sum);
 
   return 0;
